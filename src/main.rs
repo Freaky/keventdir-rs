7c3f052de3d36896eb6889d1ed327489e0adfed1
Thomas Hurst <tom@hur.st>
2018-09-10 21:30:26 +0100

    add_dir/remove_dir

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ extern crate kqueue_sys;
 extern crate libc;
 extern crate walkdir;
 
-use kqueue_sys::*;
 use kqueue_sys::constants::EventFilter::*;
 use kqueue_sys::constants::*;
+use kqueue_sys::*;
 use walkdir::{DirEntry, WalkDir};
 
+use std::collections::BTreeMap;
 use std::collections::HashMap;
 use std::fs::File;
 use std::path::{Path, PathBuf};
@@ -16,7 +17,7 @@ use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
 struct DirWatcher {
     kq: libc::c_int,
     fd_to_path: HashMap<RawFd, PathBuf>,
-    path_to_fd: HashMap<PathBuf, RawFd>,
+    path_to_fd: BTreeMap<PathBuf, RawFd>,
 }
 
 impl DirWatcher {
@@ -29,7 +30,7 @@ impl DirWatcher {
         Some(Self {
             kq,
             fd_to_path: HashMap::new(),
-            path_to_fd: HashMap::new(),
+            path_to_fd: BTreeMap::new(),
         })
     }
 
@@ -44,11 +45,7 @@ impl DirWatcher {
                 ident: fd as usize,
                 filter: EVFILT_VNODE,
                 flags: EV_ADD | EV_CLEAR,
-                fflags: NOTE_DELETE
-                    | NOTE_WRITE
-                    | NOTE_EXTEND
-                    | NOTE_LINK
-                    | NOTE_RENAME,
+                fflags: NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_LINK | NOTE_RENAME,
                 data: 0,
                 udata: std::ptr::null_mut(),
             };
@@ -85,6 +82,26 @@ impl DirWatcher {
         }
     }
 
+    fn add_dir<P: AsRef<Path>>(&mut self, path: P) -> usize {
+        let mut added = 0;
+        WalkDir::new(path)
+            .into_iter()
+            .filter_map(|entry| entry.ok())
+            .filter(|entry| self.add(entry.path()))
+            .count()
+    }
+
+    fn remove_dir<P: AsRef<Path>>(&mut self, path: P) -> usize {
+        let to_remove = self
+            .path_to_fd
+            .range(path.as_ref().to_path_buf()..)
+            .map(|(p, _fd)| p.clone())
+            .take_while(|p| p.starts_with(&path))
+            .collect::<Vec<PathBuf>>();
+
+        to_remove.iter().filter(|entry| self.remove(entry)).count()
+    }
+
     fn next_event(&mut self) -> Option<(&Path, FilterFlag)> {
         let mut ev = kevent {
             ident: 0,
@@ -111,7 +128,9 @@ impl DirWatcher {
         }
 
         let fd = ev.ident as i32;
-        self.fd_to_path.get(&fd).map(|path| (path.as_ref(), ev.fflags))
+        self.fd_to_path
+            .get(&fd)
+            .map(|path| (path.as_ref(), ev.fflags))
     }
 
     fn close(mut self) {
@@ -129,16 +148,24 @@ impl Drop for DirWatcher {
 
 fn main() -> Result<(), String> {
     let mut watcher = DirWatcher::new().unwrap();
+    let added = watcher.add_dir("test");
+    println!("Added {}", added);
+    println!("Event: {:?}", watcher.next_event());
+    let removed = watcher.remove_dir("test");
+    println!("Removed {}", removed);
 
-    for entry in WalkDir::new("test").into_iter().filter_map(|entry| entry.ok()) {
-        if watcher.add(entry.path()) {
-            println!("Watch: {}", entry.path().display());
-        }
-    }
+    // for entry in WalkDir::new("test")
+    //     .into_iter()
+    //     .filter_map(|entry| entry.ok())
+    // {
+    //     if watcher.add(entry.path()) {
+    //         println!("Watch: {}", entry.path().display());
+    //     }
+    // }
 
-    while let Some((path, flags)) = watcher.next_event() {
-        println!("Event {:?} on {}", flags, path.display());
-    }
+    // while let Some((path, flags)) = watcher.next_event() {
+    //     println!("Event {:?} on {}", flags, path.display());
+    // }
 
     Ok(())
 }
