5ad5b34e0fe12fb56377d4c42dc660f18224f6c6
Thomas Hurst <tom@hur.st>
2018-09-10 17:59:28 +0100

    Factor into struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate kqueue_sys;
 extern crate walkdir;
+extern crate libc;
 
 use kqueue_sys::constants::*;
 use kqueue_sys::constants::EventFilter::*;
-// use kqueue_sys::constants::EventFlag::*;
-// use kqueue_sys::constants::FilterFlag::*;
 use kqueue_sys::*;
 use walkdir::{DirEntry, WalkDir};
 
@@ -12,44 +11,72 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::path::{Path, PathBuf};
 
-use std::os::unix::io::{IntoRawFd, RawFd};
+use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
 
-fn main() -> Result<(), String> {
-    let watching = WalkDir::new("test")
-        .into_iter()
-        .filter_map(|entry| entry.ok())
-        .filter_map(|entry| {
-            File::open(&entry.path())
-                .map(|fd| (fd.into_raw_fd(), entry.path().to_owned()))
-                .ok()
-        }).collect::<HashMap<RawFd, PathBuf>>();
 
-    println!("{:?}", watching);
+struct DirWatcher {
+    kq: libc::c_int,
+    fd_to_path: HashMap<RawFd, PathBuf>,
+    path_to_fd: HashMap<PathBuf, RawFd>
+}
 
-    let kq = unsafe { kqueue() };
+impl DirWatcher {
+    fn new() -> Option<Self> {
+        let kq = unsafe { kqueue() };
+        if kq < 0 {
+            return None;
+        }
 
-    if kq == -1 {
-        return Err("kqueue".into());
+        Some(Self {
+            kq,
+            fd_to_path: HashMap::new(),
+            path_to_fd: HashMap::new()
+        })
     }
 
-    for (fd, path) in &watching {
-        let mut event = kevent {
-            ident: *fd as usize,
-            filter: EVFILT_VNODE,
-            flags: EV_ADD | EV_CLEAR,
-            fflags: NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_RENAME | NOTE_CLOSE_WRITE,
-            data: 0,
-            udata: std::ptr::null_mut()
-        };
-        unsafe {
-            let v = kevent(kq, &mut event, 1, std::ptr::null_mut(), 0, std::ptr::null());
-            if v == -1 {
-                return Err("kevent register".into());
+    fn add<P: AsRef<Path>>(&mut self, path: P) -> bool {
+        let path = path.as_ref();
+        if self.path_to_fd.contains_key(path) {
+            return false;
+        }
+
+        if let Ok(fd) = File::open(path)
+                .map(|fd| fd.into_raw_fd())
+        {
+            let mut event = kevent {
+                ident: fd as usize,
+                filter: EVFILT_VNODE,
+                flags: EV_ADD | EV_CLEAR,
+                fflags: NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_RENAME | NOTE_CLOSE_WRITE,
+                data: 0,
+                udata: std::ptr::null_mut()
+            };
+
+            let v = unsafe { kevent(self.kq, &mut event, 1, std::ptr::null_mut(), 0, std::ptr::null()) };
+            if v != -1 {
+                self.fd_to_path.insert(fd, path.to_owned());
+                self.path_to_fd.insert(path.to_owned(), fd);
+                return true;
             }
+
+        }
+
+        false
+    }
+
+    fn remove<P: AsRef<Path>>(&mut self, path:P) -> bool {
+        let path = path.as_ref();
+        if let Some(fd) = self.path_to_fd.remove(path) {
+            self.fd_to_path.remove(&fd);
+            // last close deletes the event automatically
+            unsafe { File::from_raw_fd(fd) };
+            true
+        } else {
+            false
         }
     }
 
-    loop {
+    fn next_event(&mut self) -> Option<&Path> {
         let mut ev = kevent {
             ident: 0,
             filter: EVFILT_VNODE,
@@ -58,15 +85,34 @@ fn main() -> Result<(), String> {
             data: 0,
             udata: std::ptr::null_mut()
         };
-        let ret = unsafe { kevent(kq, std::ptr::null_mut(), 0, &mut ev, 1, std::ptr::null()) };
+
+        let ret = unsafe { kevent(self.kq, std::ptr::null_mut(), 0, &mut ev, 1, std::ptr::null()) };
 
         if ret == -1 {
-            return Err("kevent watch".into());
+            return None;
         }
 
         let fd = ev.ident as i32;
+        Some(&self.fd_to_path[&fd])
 
-        println!("event on: {}", watching.get(&fd).map(|path| path.display()).unwrap());
+        // println!("event on: {}", watching.get(&fd).map(|path| path.display()).unwrap());
+    }
+}
+
+
+fn main() -> Result<(), String> {
+    let mut watcher = DirWatcher::new().unwrap();
+
+    for entry in WalkDir::new("test").into_iter() {
+        if let Ok(entry) = entry {
+            if watcher.add(entry.path()) {
+                println!("Watch: {}", entry.path().display());
+            }
+        }
+    }
+
+    while let Some(path) = watcher.next_event() {
+        println!("Event on {}", path.display());
     }
 
     Ok(())
